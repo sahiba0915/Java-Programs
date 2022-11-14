7d18471ca1f946ebfa9dc1398fe9b9f3d0936b18
Sahiba Kumari <sahibakumari0915@outlook.com>
2022-11-14 07:56:31 +0530

    Loops

---
+++ first-java-program/src/com/code/Loops.java
@@ -0,0 +1,28 @@
+package com.code;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+//        For loop
+//        for( int num =1; num<=5; num+=1){
+//            System.out.println(num);
+
+        Scanner in = new Scanner(System.in);
+//            int number = in.nextInt();
+//            for(int i = 1; i<=number; i+=1){
+//                System.out.println("hi");
+
+//         While loop
+//                int num = 1 ;
+//                while(num<=5) {
+//                    System.out.println(num);
+//                    num+=1;
+
+//          Do while loop
+        int n = 1;
+        do {
+            System.out.println(n);
+        }while (n!=1);
+    }
+}
