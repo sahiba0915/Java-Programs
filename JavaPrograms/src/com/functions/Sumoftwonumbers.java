5d9a7ec4e0bbbf0897c7cba3901dda03e346731e
Sahiba Kumari <sahibakumari0915@outlook.com>
2022-11-17 05:58:28 +0530

    sum using return type

--- JavaPrograms/src/com/functions/Sumoftwonumbers.java
+++ JavaPrograms/src/com/functions/Sumoftwonumbers.java
@@ -2,11 +2,24 @@ package com.functions;
 
 import java.util.Scanner;
 
-public class functionsandmethods {
+public class Sumoftwonumbers {
     public static void main(String[] args) {
         sum();
+
+     int ans = sum2();
+        System.out.println(ans);
     }
 
+    static int sum2() {
+        Scanner in = new Scanner(System.in);
+        int num1, num2, sum;
+        System.out.println("Enter the first number: ");
+        num1 = in.nextInt();
+        System.out.println("Enter the second number: ");
+        num2 = in.nextInt();
+        sum = (num1 + num2);
+        return sum;
+    }
     static void sum() {
         Scanner in = new Scanner(System.in);
         int num1, num2, sum;
