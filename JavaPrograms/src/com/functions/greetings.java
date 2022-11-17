495761d692c5d103443d4df9a85b815ad629f271
Sahiba Kumari <sahibakumari0915@outlook.com>
2022-11-17 06:19:17 +0530

    simple greeting message

---
+++ JavaPrograms/src/com/functions/greetings.java
@@ -0,0 +1,10 @@
+package com.functions;
+
+public class greetings {
+    public static void main(String[] args) {
+        greeting();
+    }
+    static void greeting() {
+        System.out.println("Hello World");
+    }
+}
