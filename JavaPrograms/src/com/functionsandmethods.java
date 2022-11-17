9ebc00af72f4e7a1c7f2cd6627172335f0a0a294
Sahiba Kumari <sahibakumari0915@outlook.com>
2022-11-17 05:48:57 +0530

    functions and methods

---
+++ JavaPrograms/src/com/functionsandmethods.java
@@ -0,0 +1,20 @@
+package com;
+
+import java.util.Scanner;
+
+public class functionsandmethods {
+    public static void main(String[] args) {
+        sum();
+    }
+
+    static void sum() {
+        Scanner in = new Scanner(System.in);
+        int num1, num2, sum;
+        System.out.println("Enter the first number: ");
+        num1 = in.nextInt();
+        System.out.println("Enter the second number: ");
+        num2 = in.nextInt();
+        sum = (num1 + num2);
+        System.out.println("The sum is: " + sum);
+    }
+}
