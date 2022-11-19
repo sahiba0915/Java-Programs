3a1daaacf525d2e9b8c57cd7c429fdef7d0679a9
Sahiba Kumari <sahibakumari0915@outlook.com>
2022-11-19 09:41:11 +0530

    reverse an array

--- JavaPrograms/src/com/arrays/Swapping.java
+++ JavaPrograms/src/com/arrays/Swapping.java
@@ -9,6 +9,19 @@ public class Swapping {
         System.out.println(Arrays.toString(arr));
         swap(arr, 3, 2);
         System.out.println(Arrays.toString(arr));
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<end){
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
     }
     static void swap(int[] arr, int index1, int index2){
         int temp = arr[index1];
