7d59f27955f93c511b10098e1f1c03caf7656b14
Sahiba Kumari <sahibakumari0915@outlook.com>
2022-11-19 08:41:54 +0530

    Passing in the functions

---
+++ JavaPrograms/src/com/arrays/PassingInFunctions.java
@@ -0,0 +1,17 @@
+package com.arrays;
+
+import java.util.Arrays;
+
+public class PassingInFunctions {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println(Arrays.toString(arr));
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    static void change(int[] arr){
+        arr[0] = 99;
+    }
+}
