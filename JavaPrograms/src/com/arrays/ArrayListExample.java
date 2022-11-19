011f24a6ab27ba5b54a6392688fd23cc9da0700f
Sahiba Kumari <sahibakumari0915@outlook.com>
2022-11-19 09:11:34 +0530

    Intro ArrayList

---
+++ JavaPrograms/src/com/arrays/ArrayListExample.java
@@ -0,0 +1,17 @@
+package com.arrays;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(233);
+        list.add(223);
+        list.add(243);
+        list.add(253);
+        list.add(263);
+
+        System.out.println(list);
+
+    }
+}
