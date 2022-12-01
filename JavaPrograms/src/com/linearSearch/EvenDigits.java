ffffc6868402b6a78ed3a59027b815a0ae8b1538
Sahiba Kumari <sahibakumari0915@outlook.com>
2022-12-01 21:09:35 +0530

    Even Number of digits

--- JavaPrograms/src/com/linearSearch/EvenDigits.java
+++ JavaPrograms/src/com/linearSearch/EvenDigits.java
@@ -2,7 +2,7 @@ package com.linearSearch;
 
 public class EvenDigits {
     public static void main(String[] args) {
-        int[] nums = {12,345,2,6,7896, 46};
+        int[] nums = {12,345,2,6,7896, 46, -56};
         System.out.println(findNumbers(nums));
 
     }
@@ -24,6 +24,10 @@ public class EvenDigits {
     }
 
     static int digits(int num){
+
+        if(num < 0){
+            num = num * -1;
+        }
         int count = 0;
         while(num > 0){
             count++;
